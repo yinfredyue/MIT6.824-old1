de6c2313afd65d588dee03373bf946d538e959e3
Yue Yin <yy0125@connect.hku.hk>
2020-03-17 18:08:32 -0500

    Finish Lab2A

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,13 +1,31 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"runtime"
+)
 
-// Debugging
 const Debug = 0
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
+func trace() {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	log.Printf("%s:%d %s\n", frame.File, frame.Line, frame.Function)
+}
+
+func DPrintf(format string, a ...interface{}) {
+	if Debug >= 1 {
 		log.Printf(format, a...)
 	}
 	return
+}
+
+func min(a int, b int) int {
+	if a <= b {
+		return a
+	}
+
+	return b
 }
